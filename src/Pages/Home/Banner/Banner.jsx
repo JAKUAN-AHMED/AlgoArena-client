05f084e59cc23d28a95840be2dcbca2a85921ec9
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2024-11-06 14:12:21 +0600

    set up successfully and made banner and navbar and home page also

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,55 @@
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import 'react-awesome-slider/dist/styles.css';
+import img1 from '../../../assets/s1.jpg';
+import img2 from '../../../assets/s2.jpg';
+import img3 from '../../../assets/s3.jpg';
+import img4 from '../../../assets/s4.jpg';
+import img5 from '../../../assets/s5.jpg';
+const images=[img1,img2,img3,img4,img5];
+const Banner = () => {
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
+  return (
+    <div className="relative">
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false}
+        interval={3000}
+        className="h-full overflow-hidden"
+      >
+        {images.map((img, idx) => (
+          <div key={idx} data-src={img} className="bg-cover bg-center h-full" />
+        ))}
+      </AutoplaySlider>
+
+      {/* banner content */}
+
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-white bg-black bg-opacity-50 font-jost">
+        {/* for text only */}
+        <div className="relative  z-20 text-center max-w-4xl px-4">
+          <h2 className="text-lg md:text-3xl text-blue-200 font-bold md:p-8 font-cinzel">
+            Master Competetive Programming with Mates
+          </h2>
+          <p className="text-[15px] md:text-xl mb-6">
+            Join forces to solve challenges,
+            <span className='text-yellow-200'>prepare for contests,and track your progress</span>
+          </p>
+        </div>
+
+        {/*search bar */}
+        <div className="flex z-20 items-center justify-center max-w-lg md:w-full">
+          <input
+            type="text"
+            className="w-full px-4 py-2 rounded-l-md text-gray-800 "
+            placeholder="Search for contests ...."
+          />
+          <button className="bg-purple-600 hover:bg-purple-700 px-4 py-2 text-white font-semibold">
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { IoIosClose, IoIosMenu } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const [open,setOpen]=useState(false);
+    return (
+      <section className="bg-purple-800 text-white p-4 font-cinzel">
+        <nav className="flex  justify-between items-center mt-4 px-4">
+          <h2 className="text-base md:text-3xl text-start">AlgoArena</h2>
+
+          {/* desktop view */}
+          <ul className="hidden md:flex gap-8">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/login"}>Login</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/all_contest"}>All Contest</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/nothing"}>Nothing</NavLink>
+            </li>
+          </ul>
+
+          {/* phone view */}
+          <div onClick={() => setOpen(!open)} className="md:hidden">
+            <button>
+              {open ? <IoIosClose size={24}/>: <IoIosMenu size={24}/> }
+            </button>
+          </div>
+        </nav>
+
+        {/* drop downmenu for mobile */}
+        {open && (
+          <div className="md:hidden">
+            <ul className="flex flex-col bg-gray-700 p-4 mt-2 rounded">
+              <li className="py-2">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className="py-2">
+                <NavLink to={"/login"}>Login</NavLink>
+              </li>
+              <li className="py-2">
+                <NavLink to={"/all_contest"}>All Contest</NavLink>
+              </li>
+              <li className="py-2">
+                <NavLink to={"/nothing"}>Nothing</NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+      </section>
+    );
+};
+
+export default Navbar;
